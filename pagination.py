ee37a21f2eed32bbe67d0d4b574f3583bea2284e
ti-bui <tibuiii3011@gmail.com>
2025-01-31 20:48:18 -0500

    pagination with mod solution

--- pagination.py
+++ pagination.py
@@ -4,10 +4,10 @@
 
 
 # Edge Cases:
-# Empty input array of numbers -> numbers <= 0
-# Invalid page length -> page_length <= 0
-# Invalid page number -> page_number <= 0
-# Page number beyond the available pages -> page_number > total pages (use mod to wrap back to valid pages)
+#   Empty input array of numbers -> numbers <= 0
+#   Invalid page length -> page_length <= 0
+#   Invalid page number -> page_number <= 0
+#   Page number beyond the available pages -> page_number > total pages (use mod to wrap back to valid pages)
 
 def pagination(numbers, page_length, page_number):
     if not numbers or page_length <= 0 or page_number <= 0:
@@ -29,5 +29,30 @@ numbers = [1, 7, 5, 2, 3, 4, 9, 8]
 #                        p1  p2  p3  p4
 # pagination(numbers, 2, 4) -> result: [9,8]
 # pagination(numbers, 2, 10) -> result: []
-
+print("1st SOLUTION")
+print(pagination(numbers, 2, 4))
 print(pagination(numbers, 2, 10))
+
+
+def pagination_mod(numbers, page_length, page_number):
+    if not numbers or page_length <= 0 or page_number <= 0:
+        return []
+
+    total_pages = (len(numbers) + page_length - 1) // page_length
+
+    adjusted_page = ((page_number-1) % total_pages) + 1
+
+    start_idx = (adjusted_page - 1) * page_length
+    end_idx = start_idx + page_length
+
+    return numbers[start_idx:end_idx]
+
+
+print("2nd SOLUTION")
+print(pagination_mod(numbers, 2, 4))
+print(pagination_mod(numbers, 2, 10))
+# Example test case:
+#           numbers = [1,7,5,2,3,4,9,8]
+#                        p1  p2  p3  p4
+# pagination(numbers, 2, 4) -> result: [9,8]
+# pagination(numbers, 2, 10) -> result: [5,2]
